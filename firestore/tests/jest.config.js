43426c41e8ef232a04ec1b7e6a3f567c865eeb6c
Jesús Rodríguez <jjesusrdz35@gmail.com>
2025-09-22 15:22:15 -0600

    rules: define hasAdminRole/hasComplianceRole; fix rules Jest config to ts-jest CJS; set emulator host/port in tests; guard cleanup

--- firestore/tests/jest.config.js
+++ firestore/tests/jest.config.js
@@ -1,18 +1,17 @@
 export default {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/'],
   testMatch: ['**/*.spec.ts', '**/*.test.ts'],
-  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
-      useESM: true,
+      tsconfig: '<rootDir>/tsconfig.json',
     },
   },
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(@firebase/rules-unit-testing|firebase)/)',
+    'node_modules/(?!(@firebase/rules-unit-testing)/)',
   ],
 };
