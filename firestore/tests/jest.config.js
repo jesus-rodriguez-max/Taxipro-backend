f773387144c50cf5db61bef6f0c2d60bcf13681e
Jesús Rodríguez <jjesusrdz35@gmail.com>
2025-09-21 21:59:04 -0600

    fix(ci): configure jest for typescript and firebase emulator

--- firestore/tests/jest.config.js
+++ firestore/tests/jest.config.js
@@ -1,6 +1,14 @@
 export default {
+  preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/"],
   testMatch: ["**/*.test.(js|ts)"],
-  transform: {}
+  transform: {
+    "^.+\.ts$": "ts-jest",
+    "^.+\.js$": "babel-jest",
+  },
+  moduleNameMapper: {
+    '^(\.{1,2}/.*)\.js
+: '$1',
+  },
 };
