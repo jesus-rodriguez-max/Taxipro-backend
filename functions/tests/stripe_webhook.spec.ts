913a5e0aef23a62e33f217d7cc1dcbabca140a0f
Jesús Rodríguez <jjesusrdz35@gmail.com>
2025-09-21 22:18:25 -0600

    fix(tests): update tsconfig modules and mock functions.config()

--- functions/tests/stripe_webhook.spec.ts
+++ functions/tests/stripe_webhook.spec.ts
@@ -5,6 +5,14 @@ import { stripe as stripeService } from '../src/stripe/service';
 import * as admin from 'firebase-admin';
 import { TripStatus } from '../src/lib/types';
 
+jest.mock('firebase-functions', () => ({
+  config: jest.fn(() => ({
+    stripe: {
+      secret: 'sk_test_mock_secret',
+    },
+  })),
+}));
+
 // Mock Firebase Admin
 jest.mock('firebase-admin', () => {
   const mockUpdate = jest.fn();
